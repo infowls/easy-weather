5c4cb847da1504173cebc08615bef468ba603fc9
infowls <online.beetle@outlook.com>
2022-02-22 23:17:31 +0100

    package.json pls work

--- server.js
+++ server.js
@@ -1,29 +1,11 @@
-var express = require('express');
-var bodyParser = require('body-parser')
-var path = require('path');
-var serveStatic = require('serve-static');
-var cors = require('cors');
+const express = require("express");
+const port = process.env.PORT || 8080;
+const app = express();
 
-var app = express();
-app.use(serveStatic(__dirname + "/dist"));
-app.use(cors());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://project-services-api.herokuapp.com");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-var port = process.env.PORT || 5000;
-// request handlers
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Enable CORS in Node.js - Clue Mediator' });
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+  res.sendFile(__dirname + "/dist/index.html");
 });
- 
-app.listen(port, () => {
-  console.log('Server started on: ' + port);
-});
+app.listen(port);
 
-
-
+console.log("Server started...");
